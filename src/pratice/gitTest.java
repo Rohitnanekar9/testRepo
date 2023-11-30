28cc5673af9fefd6738e536805b34798e481682b
Rohit Nanekar <>
2023-11-30 23:16:39 +0530

    second line commited

--- src/pratice/gitTest.java
+++ src/pratice/gitTest.java
@@ -4,6 +4,7 @@ public class gitTest {
 
 	public static void main(String[] args) {
 		System.out.println("Hello GIT");
+		System.out.println("Hello Gitttttt");
 		// TODO Auto-generated method stub
 
 	}
